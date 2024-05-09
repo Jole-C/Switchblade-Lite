b592ab2442f2356c722a90843d00774463aad95e
Jole-C <jolechallinor3@gmail.com>
2024-05-09 17:03:15 +0100

    Added bullet class
    Added bullet class with basic movement and parameters, and a circle for rendering

---
+++ game/objects/bullet/bullet.lua
@@ -0,0 +1,26 @@
+local gameobject = require "game.objects.gameobject"
+
+local bullet = class{
+    __includes = gameobject
+
+    speed = 0,
+    angle = 0,
+    damage = 0,
+
+    init = function(self, x, y, speed, angle, damage)
+        gameobject.init(self, x, y)
+
+        self.speed = speed
+        self.angle = angle
+        self.damage = damage
+    end,
+
+    update = function(self, dt)
+        self.position.x = self.position.x + math.cos(self.angle) * self.speed
+        self.position.y = self.position.y + math.sin(self.angle) * self.speed
+    end,
+
+    draw = function(self)
+        love.graphics.circle("fill", self.position.x, self.position.y, 15)
+    end,
+}
