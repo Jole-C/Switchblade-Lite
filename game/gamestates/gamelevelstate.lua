7cdf9e0f645b4daa936ae4ceaa9fcc84ada87de5
Jole-C <jolechallinor3@gmail.com>
2024-05-09 22:05:13 +0100

    Added object addition/removal to gamestate
    Game level state now has dedicated functions for object addition and removal

--- game/gamestates/gamelevelstate.lua
+++ game/gamestates/gamelevelstate.lua
@@ -6,18 +6,30 @@ gamelevelstate.world = {}
 
 function gamelevelstate:enter()
     self.world = bump.newWorld()
-    
+
     local newPlayer = player(0, 0)
-    table.insert(self.objects, newPlayer)
+    self:addObject(newPlayer)
 end
 
 function gamelevelstate:update(dt)
     for index,object in ipairs(self.objects) do
+        if object.markedForDelete == true then
+            self:removeObject(object)
+        end
+
         object:update(dt)
     end
 end
 
 function gamelevelstate:draw()
+end
+
+function gamelevelstate:addObject(object)
+    table.insert(self.objects, object)
+end
+
+function gamelevelstate:removeObject(object)
+    table.remove(self.objects, object)
 end
 
 return gamelevelstate
