f7c89c104377148438c137d0ee82943c7af586c2
Jole-C <jolechallinor3@gmail.com>
2024-05-10 19:08:02 +0100

    Fixed crash in game level state
    Fixed bug with game level state that would crash the game upon removal of an object

--- game/gamestates/gamelevelstate.lua
+++ game/gamestates/gamelevelstate.lua
@@ -30,10 +30,10 @@ end
 function gamelevelstate:update(dt)
     for index,object in ipairs(self.objects) do
         if object.markedForDelete == true then
-            self:removeObject(object)
+            self:removeObject(index)
+        else
+            object:update(dt)
         end
-
-        object:update(dt)
     end
 end
 
@@ -44,8 +44,8 @@ function gamelevelstate:addObject(object)
     table.insert(self.objects, object)
 end
 
-function gamelevelstate:removeObject(object)
-    table.remove(self.objects, object)
+function gamelevelstate:removeObject(index)
+    table.remove(self.objects, index)
 end
 
 return gamelevelstate
