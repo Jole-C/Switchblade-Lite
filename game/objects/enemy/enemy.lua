f55878273a021a5989c8480e51677d6f081b2716
Jole-C <jolechallinor3@gmail.com>
2024-05-10 18:24:47 +0100

    Added on hit function to enemy
    Comes with timer and associated variables for invulnerability cooldown

--- game/objects/enemy/enemy.lua
+++ game/objects/enemy/enemy.lua
@@ -5,6 +5,9 @@ local enemy = class{
 
     name = "enemy base",
     health = 0,
+    invulnerableTime = 0.5,
+    isInvulnerable = false,
+    invulnerableTimer,
 
     init = function(self, x, y)
         gameobject.init(self, x, y)
@@ -12,8 +15,29 @@ local enemy = class{
         self.colliderDefinition = colliderDefinitions.enemy
     end,
 
-    onHit = function(self, damage)
+    update = function(self, dt)
+        if self.invulnerableTimer then
+            self.invulnerableTimer:update()
+        end
+    end,
 
+    onHit = function(self, damage)
+        if self.isInvulnerable == true then
+            return
+        end
+
+        self.health = self.health - damage
+
+        if self.health <= 0 then
+            self:destroy()
+        end
+
+        self.isInvulnerable = true
+        self.invulnerableTimer = timer.after(self.invulnerableTime, function() self:setVulnerable() end)
+    end,
+
+    setVulnerable = function(self)
+        self.isInvulnerable = false
     end
 }
 
