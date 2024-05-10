3ca52762154d54e16141d994cda13f732db4242a
Jole-C <jolechallinor3@gmail.com>
2024-05-10 16:39:59 +0100

    Changed player to make use of deltatime
    Player code now makes use of delta time, and ship parameters have been adjusted to match. Added debug text rendering of current ship temperature

--- game/objects/player/player.lua
+++ game/objects/player/player.lua
@@ -8,23 +8,24 @@ local player = class{
     maxHealth = 3,
 
     -- Movement parameters of the ship
-    steeringSpeedMoving = 0.1,
-    steeringSpeedStationary = 0.2,
-    accelerationSpeed = 0.005,
-    boostingAccelerationSpeed = 0.02,
-    friction = 0.99,
+    steeringSpeedMoving = 6,
+    steeringSpeedStationary = 14,
+    accelerationSpeed = 3,
+    boostingAccelerationSpeed = 4,
+    friction = 59.4,
     maxSpeed = 3,
     maxBoostingSpeed = 6,
     maxShipTemperature = 100,
-    shipHeatAccumulation = 1,
-    shipCoolingRate = 1,
-    shipOverheatCoolingRate = 0.4,
+    shipHeatAccumulationRate = 1,
+    shipCoolingRate = 40,
+    shipOverheatCoolingRate = 20,
 
     -- Firing parameters of the ship
     fireCooldown = 0.05,
     bulletSpeed = 5,
     bulletDamage = 3,
     maxAmmo = 30,
+    shipKnockackForce = 0.25,
 
     -- Ship variables
     health = 3,
@@ -67,10 +68,10 @@ local player = class{
 
             -- Apply a forward thrust to the ship
             if love.keyboard.isDown("w") then
-                self.movementSpeed = self.movementSpeed + self.accelerationSpeed
+                self.movementSpeed = self.movementSpeed + (self.accelerationSpeed * dt)
                 self.movementSpeed = math.clamp(self.movementSpeed, 0, self.maxSpeed)
 
-                self.velocity = self.velocity + movementDirection * self.movementSpeed
+                self.velocity = self.velocity + movementDirection * (self.movementSpeed * dt)
 
                 self.steeringSpeed = self.steeringSpeedMoving
             end
@@ -78,20 +79,23 @@ local player = class{
             -- Boost the ship
             if love.keyboard.isDown("lshift") then
                 self.isBoosting = true
-                self.movementSpeed = self.movementSpeed + self.boostingAccelerationSpeed
-                self.velocity = self.velocity + movementDirection * self.movementSpeed
+                self.movementSpeed = self.movementSpeed + (self.boostingAccelerationSpeed * dt)
+                self.movementSpeed = math.clamp(self.movementSpeed, 0, self.maxBoostingSpeed)
+
+                self.velocity = self.velocity + movementDirection * (self.movementSpeed * dt)
+
                 self.steeringSpeed = self.steeringSpeedMoving
 
-                self.shipTemperature = self.shipTemperature + self.shipHeatAccumulation
+                self.shipTemperature = self.shipTemperature + self.shipHeatAccumulationRate
             end
 
             -- Steer the ship
             if love.keyboard.isDown("a") and self.isBoosting == false then
-                self.angle = self.angle - self.steeringSpeed
+                self.angle = self.angle - (self.steeringSpeed * dt)
             end
 
             if love.keyboard.isDown("d") and self.isBoosting == false then
-                self.angle = self.angle + self.steeringSpeed
+                self.angle = self.angle + (self.steeringSpeed * dt)
             end
 
             -- Fire gun
@@ -102,6 +106,8 @@ local player = class{
                 self.canFire = false
                 self.fireResetTimer = timer.after(self.fireCooldown, function() self:setCanFire() end)
                 self.ammo = self.ammo - 1
+
+                self.velocity = self.velocity + (movementDirection * -1) * (self.shipKnockackForce * dt)
             end
         end
 
@@ -122,7 +128,7 @@ local player = class{
         end
 
         if self.isBoosting == false then
-            self.shipTemperature = self.shipTemperature - coolingRate
+            self.shipTemperature = self.shipTemperature - (coolingRate * dt)
         end
 
         self.shipTemperature = math.clamp(self.shipTemperature, 0, self.maxShipTemperature)
@@ -137,8 +143,8 @@ local player = class{
         self.velocity = self.velocity:trimmed(trimmedSpeed)
         self.position = self.position + self.velocity
 
-        self.movementSpeed = self.movementSpeed * self.friction
-        self.velocity = self.velocity * self.friction
+        self.movementSpeed = self.movementSpeed * (self.friction * dt)
+        self.velocity = self.velocity * (self.friction * dt)
 
         -- Wrap the ship's position
         if self.position.x < 0 then
@@ -174,6 +180,8 @@ local player = class{
         yOffset = yOffset/2
 
         love.graphics.draw(self.sprite, self.position.x, self.position.y, self.angle, 1, 1, xOffset, yOffset)
+
+        love.graphics.print(self.shipTemperature, 0, 0)
     end,
 
     setCanFire = function(self)
