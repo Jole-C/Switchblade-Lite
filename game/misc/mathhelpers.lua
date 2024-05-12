f673bbb6bbbc24eb3f64d785ead28ee0bc9cbc71
Jole-C <jolechallinor3@gmail.com>
2024-05-12 21:50:23 +0100

    Added math.lerp function

--- game/misc/mathhelpers.lua
+++ game/misc/mathhelpers.lua
@@ -3,3 +3,7 @@ function math.clamp(val, lower, upper)
     if lower > upper then lower, upper = upper, lower end -- swap if boundaries supplied the wrong way
     return math.max(lower, math.min(upper, val))
 end
+
+function math.lerp(val, pos, perc)
+    return (1 - perc) * val + perc * pos
+end
