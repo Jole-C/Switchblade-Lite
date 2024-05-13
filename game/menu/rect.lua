1d9612d12a863767ee00b8d01cd825e7a356b2de
Jole-C <jolechallinor3@gmail.com>
2024-05-13 20:16:02 +0100

    Added rectangle hud element

---
+++ game/menu/rect.lua
@@ -0,0 +1,25 @@
+local hudElement = require "game.render.hudelement"
+
+local rect = class{
+    __includes = hudElement,
+    
+    position = {},
+    dimensions = {},
+    fillType = "fill",
+    colour = {},
+
+    init = function(self, x, y, w, h, fillType, colour)
+        self.position = vector.new(x, y)
+        self.dimensions = vector.new(w, h)
+        self.fillType = fillType
+        self.colour = colour
+    end,
+
+    draw = function(self)
+        love.graphics.setColor(self.colour)
+        love.graphics.rectangle(self.fillType, self.position.x, self.position.y, self.dimensions.x, self.dimensions.y)
+        love.graphics.setColor(1, 1, 1, 1)
+    end
+}
+
+return rect
