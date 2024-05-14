1a4f45d90400363f1137c266ea713271d9772b97
Jole-C <jolechallinor3@gmail.com>
2024-05-14 17:05:18 +0100

    Added proper options to main menu

--- game/menu/mainmenu/mainmenu.lua
+++ game/menu/mainmenu/mainmenu.lua
@@ -2,6 +2,7 @@ local menu = require "game.menu.menu"
 local textButton = require "game.interface.textbutton"
 local text = require "game.interface.text"
 local toggleButton = require "game.interface.togglebutton"
+local slider = require "game.interface.slider"
 
 local mainMenu = class{
     __includes = menu,
@@ -12,6 +13,7 @@ local mainMenu = class{
             ["main"] = 
             {
                 displayMenuName = false,
+                
                 elements =
                 {
                     textButton("start", "font main", 10, 10, 15, 10, function(self)
@@ -33,19 +35,24 @@ local mainMenu = class{
             ["options"] = 
             {
                 displayMenuName = false,
+
                 elements =
                 {
                     text("visual", "font main", false, 10, 10),
 
-                    toggleButton("boop a doop", "font main", 10, 20, 15, 20),
-                    
-                    text("audio", "font main", false, 10, 30),
+                    toggleButton("toggle bg.", "font main", 10, 25, 20, 25),
 
-                    toggleButton("boop a doop", "font main", 10, 40, 15, 40),
+                    slider("bg. fading", "font main", 25, 0, 100, 10, 40),
 
-                    text("controls", "font main", false, 10, 50),
+                    slider("bg. speed", "font main", 25, 0, 100, 10, 55),
 
-                    textButton("back", "font main", 10, 60, 15, 60, function(self)
+                    text("audio", "font main", false, 10, 80),
+
+                    slider("music vol.", "font main", 25, 0, 100, 10, 95),
+
+                    slider("sfx vol.", "font main", 25, 0, 100, 10, 110),
+
+                    textButton("back", "font main", 10, 130, 15, 130, function(self)
                         if self.owner then
                             self.owner:switchMenu("main")
                         end
