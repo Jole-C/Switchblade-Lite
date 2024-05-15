6a5a66737cb656e56d38c0837908fcd4b5cd013e
Jole-C <jolechallinor3@gmail.com>
2024-05-15 20:43:08 +0100

    Adjusted player hud to ceil/abs ammo count

--- game/objects/player/playerhuddisplay.lua
+++ game/objects/player/playerhuddisplay.lua
@@ -19,7 +19,7 @@ local playerHud = class{
 
         love.graphics.print(math.floor(self.playerReference.shipTemperature), 10, 10)
         love.graphics.print(self.playerReference.health, 10, 20)
-        love.graphics.print(self.playerReference.ammo, 10, 30)
+        love.graphics.print(math.abs(math.ceil(self.playerReference.ammo)), 10, 30)
     end
 }
 
