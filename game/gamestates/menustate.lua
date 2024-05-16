c28e7b5a12fc1c4a6df15ea3e90ab6bdafcb3f35
Jole-C <jolechallinor3@gmail.com>
2024-05-16 18:30:01 +0100

    Removed unnecessary code from menu state

--- game/gamestates/menustate.lua
+++ game/gamestates/menustate.lua
@@ -4,48 +4,25 @@ local menuState = gamestate.new()
 
 function menuState:init()
     self.name = "menu"
-    self.objects = {}
 end
 
 function menuState:enter()
     interfaceRenderer:clearElements()
-
-    local menu = mainMenu()
-    self:addObject(menu)
+    self.menu = mainMenu()
 end
 
 function menuState:leave()
-    for i = 1, #self.objects do
-        local object = self.objects[i]
-        object:destroy()
-    end
-
-    self.objects = {}
+    self.menu:destroy()
+    self.menu = nil
     interfaceRenderer:clearElements()
 end
 
 function menuState:update(dt)
-    for index,object in ipairs(self.objects) do
-        if object.markedForDelete == true then
-            self:removeObject(index)
-        else
-            object:update(dt)
-        end
-    end
+    self.menu:update(dt)
 end
 
 function menuState:draw()
-    for index,object in ipairs(self.objects) do
-        object:draw(dt)
-    end
-end
-
-function menuState:addObject(object)
-    table.insert(self.objects, object)
-end
-
-function menuState:removeObject(index)
-    table.remove(self.objects, index)
+    self.menu:draw()
 end
 
 return menuState
