00a00d5eca70887dd68a2875a23bafc2f50dcd1b
Jole-C <jolechallinor3@gmail.com>
2024-05-18 16:01:03 +0100

    Added center text to text button

--- game/interface/textbutton.lua
+++ game/interface/textbutton.lua
@@ -4,17 +4,26 @@ local textButton = class{
     __includes = menuButton,
 
     text = "",
+    centerText,
 
-    init = function(self, text, font, restX, restY, selectedX, selectedY, execute)
+    init = function(self, text, font, restX, restY, selectedX, selectedY, execute, centerText)
+        centerText = centerText or false
+
         menuButton.init(self, restX, restY, selectedX, selectedY, execute)
         self.font = resourceManager:getResource(font)
         self.text = text
+        self.centerText = centerText
     end,
 
     draw = function(self)
         love.graphics.setColor(self.drawColour)
         love.graphics.setFont(self.font)
-        love.graphics.print(self.text, self.position.x, self.position.y)
+
+        if self.centerText then
+            love.graphics.printf(self.text, self.position.x, self.position.y, gameWidth, "center")
+        else
+            love.graphics.print(self.text, self.position.x, self.position.y)
+        end
 
         love.graphics.setColor(1, 1, 1, 1)
     end
