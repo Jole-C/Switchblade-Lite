111902ada036bad0ca74c88b94c7dea67bfb553b
Jole-C <jolechallinor3@gmail.com>
2024-05-21 19:09:23 +0100

    Removed game width/height from config

--- conf.lua
+++ conf.lua
@@ -1,10 +1,7 @@
-gameWidth = 320
-gameHeight = 180
-
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = "11.5"                -- The LÖVE version this game was made for (string)
-    t.console = true                  -- Attach a console (boolean, Windows only)
+    t.console = false                  -- Attach a console (boolean, Windows only)
  
     t.window.title = "Switchblade" -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
