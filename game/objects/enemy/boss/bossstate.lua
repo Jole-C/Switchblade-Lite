cba71966e6b75b18c7b232c4e1088ac4274dd722
Noba <jolechallinor3@gmail.com>
2024-06-07 19:04:40 +0100

    Added boss class

---
+++ game/objects/enemy/boss/bossstate.lua
@@ -0,0 +1,15 @@
+local bossState = class({name = "Boss State"})
+
+function bossState:enter(bossInstance)
+
+end
+
+function bossState:exit(bossInstance)
+
+end
+
+function bossState:update(dt, bossInstance)
+
+end
+
+return bossState
