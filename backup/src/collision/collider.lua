3d5d124de92508636f2f7a05907020d952b18c50
Noba <jolechallinor3@gmail.com>
2024-06-09 16:27:55 +0100

    Removed backup folder

--- backup/src/collision/collider.lua
+++
@@ -1,8 +0,0 @@
-local collider = class({name = "Collider"})
-
-function collider:new(colliderDefinition, owner)
-    self.colliderDefinition = colliderDefinition
-    self.owner = owner
-end
-
-return collider
--- backup/src/objects/player/playerdefinition.lua
+++
@@ -1,9 +0,0 @@
-local playerDefinition = class({name = "Player Definition"})
-
-function playerDefinition:new(shipName, shipClass, uiShipSprite)
-    self.shipName = shipName
-    self.shipClass = shipClass
-    self.uiShipSprite = uiShipSprite
-end
-
-return playerDefinition
