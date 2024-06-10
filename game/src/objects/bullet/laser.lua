83b21445dd96465e23f1170df23c29c68356142e
Noba <jolechallinor3@gmail.com>
2024-06-10 14:04:09 +0100

    Laser now properly aligns query lines

--- game/src/objects/bullet/laser.lua
+++ game/src/objects/bullet/laser.lua
@@ -1,13 +1,14 @@
 local gameObject = require "src.objects.gameobject"
 local laser = class({name = "Laser", extends = gameObject})
 
-function laser:new(x, y, angle, damage, length, lifetime)
+function laser:new(x, y, angle, damage, length, lifetime, numberOfQueries)
     self:super(x, y)
 
     self.angle = angle or 0
     self.damage = damage or 2
     self.length = length or 100
     self.lifetime = lifetime or 0.5
+    self.numberOfQueries = numberOfQueries or 3
     
     if self.sprite then
         local width, height = self.sprite:getDimensions()
@@ -25,24 +26,30 @@ function laser:update(dt)
     end
 
     local quarterAngle = 1.5708
-    local widthIncrement = self.width/3
-    local baseX = (self.position.x - math.cos(self.angle + quarterAngle) * widthIncrement)
-    local baseY = (self.position.y - math.sin(self.angle + quarterAngle) * widthIncrement)
+    local widthIncrement = self.width/self.numberOfQueries - 1
+    local halfWidth = self.width / 2
     
-    for i = 1, 3 do
-        local x1 = baseX + math.cos(self.angle + quarterAngle) * ((widthIncrement * i) - 2)
-        local y1 = baseY + math.sin(self.angle + quarterAngle) * ((widthIncrement * i) - 2)
+    for i = 1, self.numberOfQueries do
+        local offset = (i - 1) * widthIncrement - halfWidth
+
+        local x1 = self.position.x + math.cos(self.angle + quarterAngle) * offset
+        local y1 = self.position.y + math.sin(self.angle + quarterAngle) * offset  
         local x2 = x1 + math.cos(self.angle) * self.length
         local y2 = y1 + math.sin(self.angle) * self.length
         
         local world = gameHelper:getWorld()
 
         local items, len = world:querySegment(x1, y1, x2, y2)
-        self:handleCollision(items, len)
+        
+        local returnEarly = self:handleCollision(items, len, dt)
+
+        if returnEarly == true then
+            return
+        end
     end
 end
 
-function laser:handleCollision(items, length)
+function laser:handleCollision(items, length, dt)
 
 end
 
