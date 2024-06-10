b2d3050b9e51f90ab9a1d2403ef804167bd2a686
Noba <jolechallinor3@gmail.com>
2024-06-10 19:31:14 +0100

    Added enemy base class and split up enemy class
    Majority of enemy class functions are now in the enemy base class, so that boss can inherit from it

--- game/src/objects/enemy/enemy.lua
+++ game/src/objects/enemy/enemy.lua
@@ -1,87 +1,19 @@
-local gameObject = require "src.objects.gameobject"
-local enemy = class({name = "Enemy", extends = gameObject})
+local enemyBase = require "src.objects.enemy.enemybase"
+local enemy = class({name = "Enemy", extends = enemyBase})
 
 function enemy:new(x, y, spriteName)
     self:super(x, y)
 
-    -- Parameters
-    self.contactDamage = 1
-    self.health = 3
-    self.maxInvulnerableTime = 0.15
-    self.enemyColour = game.manager.currentPalette.enemyColour
-
-    -- Variables
-    self.isInvulnerable = false
-    self.invulnerableTime = 0
-
     -- Components
     self.sprite = game.resourceManager:getResource(spriteName)
-    
-    -- Register the enemy
-    local currentGamestate = gameHelper:getCurrentState()
-    if currentGamestate.enemyManager then
-        currentGamestate.enemyManager:registerEnemy(self)
-    end
 end
 
 function enemy:update(dt)
-    self.invulnerableTime = self.invulnerableTime - 1 * dt
-
-    if self.invulnerableTime <= 0 then
-        self.isInvulnerable = false
-    end
-
-    if self.isInvulnerable == true then
-        self.enemyColour = {1, 1, 1, 1}
-    else
-        self.enemyColour = game.manager.currentPalette.enemyColour
-    end
-end
-
-function enemy:handleDamage(damageType, amount)
-    if damageType == "bullet" or damageType == "boost" then
-        self.health = self.health - amount
-    end
-end
-
-function enemy:onHit(damageType, amount)
-    if self.isInvulnerable == true then
-        return
-    end
-
-    self:handleDamage(damageType, amount)
-
-    if self.health <= 0 then
-        self:destroy()
-    end
-
-    self:setInvulnerable()
-
-    if game.manager then
-        game.manager:setFreezeFrames(3)
-    end
-end
-
-function enemy:setInvulnerable()
-    self.isInvulnerable = true
-    self.invulnerableTime = self.maxInvulnerableTime
+    enemyBase.update(self, dt)
 end
 
 function enemy:cleanup()
-    local currentGamestate = gameHelper:getCurrentState()
-    currentGamestate.cameraManager:screenShake(0.1)
-
-    if game.manager then
-        game.manager:setFreezeFrames(2)
-    end
-
-    if currentGamestate.stageDirector then
-        currentGamestate.stageDirector:registerEnemyKill()
-    end
-
-    if currentGamestate.enemyManager then
-        currentGamestate.enemyManager:unregisterEnemy(self)
-    end
+    enemyBase.cleanup(self)
 end
 
 return enemy
---
+++ game/src/objects/enemy/enemybase.lua
@@ -0,0 +1,124 @@
+local gameObject = require "src.objects.gameobject"
+local enemyBase = class({name = "Enemy Base", extends = gameObject})
+
+function enemyBase:new(x, y)
+    self:super(x, y)
+
+    self.health = 1
+    self.maxInvulnerableTime = 0.15
+
+    self.invulnerableTime = 0
+    self.isInvulnerable = false
+    self.enemyColour = game.manager.currentPalette.enemyColour
+    
+    local currentGamestate = gameHelper:getCurrentState()
+
+    if currentGamestate.enemyManager then
+        currentGamestate.enemyManager:registerEnemy(self)
+    end
+end
+
+function enemyBase:update(dt)
+    self.invulnerableTime = self.invulnerableTime - 1 * dt
+
+    if self.invulnerableTime <= 0 then
+        self.isInvulnerable = false
+    end
+
+    if self.isInvulnerable == true then
+        self.enemyColour = {1, 1, 1, 1}
+    else
+        self.enemyColour = game.manager.currentPalette.enemyColour
+    end
+end
+
+function enemyBase:checkColliders(colliders)
+    local world = gameHelper:getWorld()
+    local colliderTable = colliders
+
+    if colliders ~= "table" then
+        colliderTable = {colliders}
+    end
+
+    for _, collider in ipairs(colliderTable) do
+        if world:hasItem(collider) then
+            local colliderPositionX, colliderPositionY = world:getRect(collider)
+            local x, y, cols, len = world:check(self.collider, colliderPositionX, colliderPositionY)
+    
+            for i = 1, len do
+                local collidedObject = cols[i].other.owner
+                local colliderDefinition = cols[i].other.colliderDefinition
+    
+                if not collidedObject or not colliderDefinition then
+                    goto continue
+                end
+    
+                local returnEarly = self:handleCollision(collider, collidedObject, colliderDefinition)
+    
+                if returnEarly then
+                    return
+                end
+    
+                ::continue::
+            end
+        end
+    end
+end
+
+function enemyBase:handleCollision(collider, collidedObject, colliderDefinition)
+    if colliderDefinition == colliderDefinitions.player then
+        if collidedObject.onHit then
+            collidedObject:onHit(1)
+        end
+    end
+
+    return false
+end
+
+function enemyBase:handleDamage(damageType, amount)
+    if damageType == "bullet" or damageType == "boost" or damageType == "contact" then
+        self.health = self.health - amount
+    end
+end
+
+function enemyBase:onHit(damageType, amount)
+    if self.isInvulnerable == true then
+        return
+    end
+
+    self:handleDamage(damageType, amount)
+
+    if self.health <= 0 then
+        self:destroy()
+    end
+
+    self:setInvulnerable()
+
+    if game.manager then
+        game.manager:setFreezeFrames(1)
+    end
+end
+
+function enemyBase:setInvulnerable()
+    self.isInvulnerable = true
+    self.invulnerableTime = self.maxInvulnerableTime
+end
+
+function enemyBase:cleanup()
+    local currentGamestate = gameHelper:getCurrentState()
+    currentGamestate.cameraManager:screenShake(0.1)
+
+    if game.manager then
+        game.manager:setFreezeFrames(2)
+    end
+
+    if currentGamestate.stageDirector then
+        currentGamestate.stageDirector:registerEnemyKill()
+    end
+
+    if currentGamestate.enemyManager then
+        currentGamestate.enemyManager:unregisterEnemy(self)
+    end
+end
+
+return enemyBase
