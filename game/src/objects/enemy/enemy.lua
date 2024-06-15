04ff30c9b621fc72fb6ef112a6fe454bde30707b
Noba <jolechallinor3@gmail.com>
2024-06-15 18:50:41 +0100

    Added death sounds to enemies

--- game/src/objects/enemy/enemy.lua
+++ game/src/objects/enemy/enemy.lua
@@ -6,6 +6,23 @@ function enemy:new(x, y, spriteName)
 
     -- Components
     self.sprite = game.resourceManager:getResource(spriteName)
+
+    -- Sounds
+    self.deathSounds =
+    {
+        ripple.newSound(game.resourceManager:getResource("enemy hit 1")),
+        ripple.newSound(game.resourceManager:getResource("enemy hit 2")),
+        ripple.newSound(game.resourceManager:getResource("enemy hit 3")),
+        ripple.newSound(game.resourceManager:getResource("enemy hit 4")),
+        ripple.newSound(game.resourceManager:getResource("enemy hit 5")),
+    }
+
+    self.deathSounds = {}
+
+    for i = 1, 5 do
+        table.insert(self.deathSounds, ripple.newSound(game.resourceManager:getResource("enemy hit "..i)))
+        self.deathSounds[i]:tag(game.tags.sfx)
+    end
 end
 
 function enemy:update(dt)
@@ -14,7 +31,17 @@ end
 
 function enemy:cleanup()
     enemyBase.cleanup(self)
+    self:playDeathSound()
     game.particleManager:burstEffect("Explosion", 50, self.position)
+end
+
+function enemy:playDeathSound()
+    local soundIndex = math.random(1, #self.deathSounds)
+    local sound = self.deathSounds[soundIndex]
+
+    if sound then
+        sound:play()
+    end
 end
 
 return enemy
