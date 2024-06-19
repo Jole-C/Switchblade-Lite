99bda05b6b9704cb5456b72da21f0f0687c3801b
Noba <jolechallinor3@gmail.com>
2024-06-19 13:28:20 +0100

    Boss destroys all enemies when going into unshielded state

--- game/src/objects/enemy/boss/boss1/states/unshieldintro.lua
+++ game/src/objects/enemy/boss/boss1/states/unshieldintro.lua
@@ -4,6 +4,12 @@ local unshieldIntro = class({name = "Unshield Intro", extends = bossState})
 function unshieldIntro:enter(bossInstance)
     bossInstance:setFearLevel(self.parameters.fearLevel or 1)
     bossInstance:switchState(self.parameters.returnState)
+
+    local enemyManager = gameHelper:getCurrentState().enemyManager
+
+    if enemyManager then
+        enemyManager:destroyAllEnemies({bossInstance})
+    end
 end
 
 return unshieldIntro
