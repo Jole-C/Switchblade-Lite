2587e1d2e0a57391ce5608409ea5d8040ef6f46b
Noba <jolechallinor3@gmail.com>
2024-06-20 20:03:49 +0100

    Updated player bullet with new collision checking

--- game/src/objects/player/playerbullets/playerbullet.lua
+++ game/src/objects/player/playerbullets/playerbullet.lua
@@ -1,27 +1,15 @@
 local bullet = require "src.objects.bullet.bullet"
 local playerBullet = class({name = "Player Bullet", extends = bullet})
 
-function playerBullet:checkCollision(xx, yy)
-    local world = gameHelper:getWorld()
-    
-    if world:hasItem(self.collider) then
-        local x, y, cols, len = world:check(self.collider, xx, yy)
-
-        for i = 1, len do
-            local collidedObject = cols[i].other.owner
-            local colliderDefinition = cols[i].other.colliderDefinition
-
-            if collidedObject and colliderDefinition then
-                if colliderDefinition == colliderDefinitions.enemy then
-                    if collidedObject.onHit then
-                        collidedObject:onHit("bullet", self.damage)
-                    end
-                    
-                    self:destroy()
-                end
-            end
+function playerBullet:handleCollision(collider, collidedObject, colliderDefinition)
+    if colliderDefinition == colliderDefinitions.enemy then
+        if collidedObject.onHit then
+            collidedObject:onHit("bullet", self.damage)
+            self:destroy()
         end
     end
+
+    return false
 end
 
 function playerBullet:draw()
