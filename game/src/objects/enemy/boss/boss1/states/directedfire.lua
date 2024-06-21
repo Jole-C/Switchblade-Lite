f16ca8b0ae287ded01d3662deb83d55f2e94aa76
Noba <jolechallinor3@gmail.com>
2024-06-21 17:32:57 +0100

    Fixed typo in directed fire

--- game/src/objects/enemy/boss/boss1/states/directedfire.lua
+++ game/src/objects/enemy/boss/boss1/states/directedfire.lua
@@ -1,14 +1,18 @@
-local bossState = require "src.objects.enemy.boss.bossstate"
+local bossAttack = require "src.objects.enemy.boss.bossattack"
 local charger = require "src.objects.enemy.charger"
 local sticker = require "src.objects.enemy.sticker"
 
-local fireDirected = class({name = "Fire Directed", extends = bossState})
+local fireDirected = class({name = "Fire Directed", extends = bossAttack})
 
 function fireDirected:enter(bossInstance)
+    bossAttack.enter(self, bossInstance)
+    
     self.enemiesToFire = self.parameters.enemiesToFire or 5
     self.maxFireCooldown = self.parameters.maxFireCooldown or 0.5
     self.fireCooldown = self.maxFireCooldown
     self.returnState = self.parameters.returnState
+
+    self.chosenEnemyFunction = self:chooseEnemy()
     
     bossInstance:setMandibleOpenAmount(1)
 end
@@ -22,7 +26,7 @@ function fireDirected:update(dt, bossInstance)
         self.fireCooldown = self.maxFireCooldown
 
         local newEnemy = self.chosenEnemyFunction(bossInstance.angle, bossInstance.enemySpawnPosition.x, bossInstance.enemySpawnPosition.y)
-        
+
         if newEnemy ~= nil then
             newEnemy.angle = bossInstance.angle + math.rad(math.random(-5, 5))
             gameHelper:addGameObject(newEnemy)
