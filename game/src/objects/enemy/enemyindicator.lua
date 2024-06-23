a2254897ee98bd301a064bf2c8853f2c4ae6404d
Noba <jolechallinor3@gmail.com>
2024-06-23 13:07:08 +0100

    Added enemy reference assert to enemy indicator for safety

--- game/src/objects/enemy/enemyindicator.lua
+++ game/src/objects/enemy/enemyindicator.lua
@@ -5,10 +5,11 @@ function enemyIndicator:new(x, y, enemy)
     self:super(x, y)
 
     self.enemy = enemy
+    assert(enemy ~= nil, "Enemy reference is nil!")
 
     self.circleRadiusFrequency = 3
-    self.circleRadiusAmplitude = 10
-    self.circleRadius = 10
+    self.circleRadiusAmplitude = 5
+    self.circleRadius = 5
 
     self.circleRadiusTime = 0
     self.circleRadiusOffset = 0
