ec9ce896b1a7afd48bb22b8db04f3e149942cc11
Jole-C <jolechallinor3@gmail.com>
2024-06-25 14:50:06 +0100

    Revert "Player bullets now have a chance to home in on enemies"
    This reverts commit 9a6b95b31c41e4ec4a7aba958a12660f5eea3ffe.

--- game/src/objects/player/playerbullets/playerbullet.lua
+++ game/src/objects/player/playerbullets/playerbullet.lua
@@ -1,36 +1,6 @@
 local bullet = require "src.objects.bullet.bullet"
 local playerBullet = class({name = "Player Bullet", extends = bullet})
 
-function playerBullet:new(x, y, speed, angle, damage, colliderDefinition, width, height)
-    self:super(x, y, speed, angle, damage, colliderDefinition, width, height)
-    self.targetEnemy = nil
-    self.targetEnemyAngleLerpRate = 0.02
-
-    local enemyManager = gameHelper:getCurrentState().enemyManager
-    local validEnemies = {}
-
-    if enemyManager and math.random(0, 100) > 75 then
-        for _, enemy in pairs(enemyManager.enemies) do
-            local vectorToEnemy = (enemy.position - self.position)
-            local dot = vectorToEnemy:dot(vec2(math.cos(self.angle), math.sin(self.angle)))
-
-            if dot > 0.99 then
-                table.insert(validEnemies, enemy)
-            end
-        end
-
-        self.targetEnemy = validEnemies[math.random(1, #validEnemies)]
-    end
-end
-
-function playerBullet:update(dt)
-    bullet.update(self, dt)
-
-    if self.targetEnemy then
-        self.angle = math.lerpAngle(self.angle, (self.targetEnemy.position - self.position):angle(), self.targetEnemyAngleLerpRate, dt)
-    end
-end
-
 function playerBullet:handleCollision(collider, collidedObject, colliderDefinition)
     if colliderDefinition == colliderDefinitions.enemy then
         if collidedObject.onHit then
