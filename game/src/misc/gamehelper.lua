cc9880f9a0007608360520df63d730b5538752aa
Jole-C <jolechallinor3@gmail.com>
2024-06-26 14:21:28 +0100

    Removed assertion checks from the game manager
    These assertion checks just resulted in code sometimes failing, when it should be up to the object calling these functions to make sure the return value is not nil.

--- game/src/misc/gamehelper.lua
+++ game/src/misc/gamehelper.lua
@@ -2,41 +2,26 @@ local gameHelper = {}
 
 function gameHelper:addGameObject(object)
     local currentGamestate = self:getCurrentState()
-
-    assert(object ~= nil, "Object is nil!")
-
     currentGamestate:addObject(object)
 end
 
 function gameHelper:getCurrentState()
     local currentGamestate = game.gameStateMachine:current_state()
-
-    assert(currentGamestate ~= nil, "Gamestate is nil!")
-
     return currentGamestate
 end
 
 function gameHelper:getWorld()
     local currentGamestate = self:getCurrentState()
-    
-    assert(currentGamestate.world ~= nil, "Gamestate's world is nil!")
-
     return currentGamestate.world
 end
 
 function gameHelper:getArena()
     local currentGamestate = self:getCurrentState()
-    
-    assert(currentGamestate.arena ~= nil, "Gamestate's arena is nil!")
-
     return currentGamestate.arena
 end
 
 function gameHelper:screenShake(amount)
     local currentGamestate = self:getCurrentState()
-
-    assert(currentGamestate.cameraManager ~= nil, "Gamestate's camera manager is nil!")
-
     currentGamestate.cameraManager:screenShake(amount * game.manager:getOption("screenshakeIntensity")/100)
 end
 
