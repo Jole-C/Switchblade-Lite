ce6dd50a033dedab7ce2df40c49e2bd7a1ab8f49
Noba <jolechallinor3@gmail.com>
2024-06-27 02:56:07 +0100

    Gamestates now reset zoom

--- game/src/gamestates/gameoverstate.lua
+++ game/src/gamestates/gameoverstate.lua
@@ -6,6 +6,7 @@ local gameOverState = class({name = "Gameover State", extends = gamestate})
 function gameOverState:enter()
     game.camera:setWorld(0, 0, game.arenaValues.screenWidth, game.arenaValues.screenHeight)
     game.camera:setPosition(game.arenaValues.screenWidth/2, game.arenaValues.screenHeight/2)
+    game.camera:setScale(1)
     game.manager:swapPaletteGroup("main")
     self.menu = gameoverMenu()
 end
