0bd3edf78a0b3f860cbcfa5f40aca40c95404d7f
Noba <jolechallinor3@gmail.com>
2024-06-28 16:01:26 +0100

    Fixed boss laser and improved visuals

--- game/src/objects/enemy/boss/boss1/boss1laser.lua
+++ game/src/objects/enemy/boss/boss1/boss1laser.lua
@@ -2,14 +2,22 @@ local laser = require "src.objects.bullet.laser"
 local bossLaser = class({name = "Boss 1 Laser", extends = laser})
 
 function bossLaser:new(x, y, angle, length, lifetime)
-    self.spriteOuter = game.resourceManager:getAsset("Enemy Assets"):get("boss1"):get("sprites"):get("laserOuter")
+    self.sprite = game.resourceManager:getAsset("Enemy Assets"):get("boss1"):get("sprites"):get("laserOuter")
     self.spriteInner = game.resourceManager:getAsset("Enemy Assets"):get("boss1"):get("sprites"):get("laserInner")
 
     self:super(x, y, angle, 0, length, lifetime, 8)
+
+    self.innerLaserScaleMultiplier = 0
+    self.innerLaserScaleTime = 0
+    self.innerLaserScaleFrequency = 30
+    self.innerLaserScaleAmplitude = 1.5
 end
 
 function bossLaser:update(dt)
     laser.update(self, dt)
+    
+    self.innerLaserScaleTime = self.innerLaserScaleTime + (self.innerLaserScaleFrequency * dt)
+    self.innerLaserScaleMultiplier = ((math.sin(self.innerLaserScaleTime) + 1) / 2) * self.innerLaserScaleAmplitude
 end
 
 function bossLaser:draw()
@@ -17,15 +25,15 @@ function bossLaser:draw()
         return
     end
 
-    local xOffset, yOffset = self.spriteOuter:getDimensions()
+    local xOffset, yOffset = self.sprite:getDimensions()
     xOffset = xOffset/2
     yOffset = yOffset/2
     
     love.graphics.setColor(game.manager.currentPalette.enemyColour)
-    love.graphics.draw(self.spriteOuter, self.position.x, self.position.y, self.angle, self.length, 1, 0, yOffset)
+    love.graphics.draw(self.sprite, self.position.x, self.position.y, self.angle, self.length, 1, 0, yOffset)
     
     love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.draw(self.spriteInner, self.position.x, self.position.y, self.angle, self.length, 1, 0, yOffset)
+    love.graphics.draw(self.spriteInner, self.position.x, self.position.y, self.angle, self.length, 0.5 + 1 * self.innerLaserScaleMultiplier, 0, yOffset)
     
     love.graphics.setColor(game.manager.currentPalette.enemyColour)
     love.graphics.circle("fill", self.position.x, self.position.y, 32)
