4db0e2172ff5224cef8f9cdd03014ddb60df35dd
Noba <jolechallinor3@gmail.com>
2024-07-10 21:36:41 +0100

    Increased the time given when completing a wave

--- game/src/objects/stagedirector/stagedirector.lua
+++ game/src/objects/stagedirector/stagedirector.lua
@@ -14,7 +14,7 @@ function stageDirector:new(levelDefinition)
 
     self.maxMinutes = 0
     self.maxSeconds = 30
-    self.incrementSeconds = 7
+    self.incrementSeconds = 10
     self.bossMinutes = 0
     self.bossSeconds = 0
 
