c257c5253a8cf77fa8478adb829426244c2406bb
Noba <jolechallinor3@gmail.com>
2024-07-10 21:35:15 +0100

    Updated outline shader with explicit float declarations

--- game/src/game.lua
+++ game/src/game.lua
@@ -506,10 +506,10 @@ function game:setupResources()
             vec4 effect(vec4 colour, Image texture, vec2 texturePos, vec2 screenPos)
             {
                 float alpha = 4 * texture2D(texture, texturePos).a;
-                alpha -= texture2D(texture, texturePos + (stepSize.x, 0.0)).a;
-                alpha -= texture2D(texture, texturePos + (-stepSize.x, 0.0)).a;
-                alpha -= texture2D(texture, texturePos + (0, stepSize.y)).a;
-                alpha -= texture2D(texture, texturePos + (0, -stepSize.y)).a;
+                alpha -= texture2D( texture, texturePos + vec2( stepSize.x, 0.0f ) ).a;
+                alpha -= texture2D( texture, texturePos + vec2( -stepSize.x, 0.0f ) ).a;
+                alpha -= texture2D( texture, texturePos + vec2( 0.0f, stepSize.y ) ).a;
+                alpha -= texture2D( texture, texturePos + vec2( 0.0f, -stepSize.y ) ).a;
                 
                 return vec4(1, 1, 1, alpha);
             }
