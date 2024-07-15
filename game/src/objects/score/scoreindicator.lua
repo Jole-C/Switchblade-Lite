a1508b9a774670570aaca65b26b57b518f31dc8e
Noba <jolechallinor3@gmail.com>
2024-07-15 17:57:43 +0100

    Refactored score indicator
    Score indicator's rendering code is now in a separate object for rendering generic text within the arena, with score indicator inheriting from it and passing in score/multiplier as a string in the constructor

--- game/src/objects/score/scoreindicator.lua
+++ game/src/objects/score/scoreindicator.lua
@@ -1,41 +1,9 @@
-local gameObject = require "src.objects.gameobject"
-local scoreIndicator = class({name = "Score Indicator", extends = gameObject})
+local worldIndicator = require "src.objects.stagedirector.worldalertobject"
+local scoreIndicator = class({name = "Score Indicator", extends = worldIndicator})
 
 function scoreIndicator:new(x, y, score, multiplier)
-    self:super(x, y)
-
-    self.font = game.resourceManager:getAsset("Interface Assets"):get("fonts"):get("fontScore")
-
-    self.yOffset = 20
-    self.targetY = y - self.yOffset
-    self.yLerpRate = 0.05
-    self.score = score
-    self.multiplier = multiplier
-    self.textAlpha = 1
-    self.textAlphaLerpRate = 0.05
-    
-    gameHelper:addScore(self.score, self.multiplier)
-end
-
-function scoreIndicator:update(dt)
-    self.position.y = math.lerpDT(self.position.y, self.targetY, self.yLerpRate, dt)
-
-    if math.abs(self.position.y - self.targetY) < 1 then
-        self.textAlpha = math.lerpDT(self.textAlpha, 0, self.textAlphaLerpRate, dt)
-
-        if self.textAlpha < 0.1 then
-            self:destroy()
-        end
-    end
-end
-
-function scoreIndicator:draw()
-    local width = self.font:getWidth(self.score)
-
-    local colour = game.manager.currentPalette.uiColour
-    love.graphics.setColor(colour[1], colour[2], colour[3], self.textAlpha)
-    love.graphics.print(tostring(self.score).."(x"..self.multiplier..")", self.position.x - width / 2, self.position.y)
-    love.graphics.setColor(1, 1, 1, 1)
+    self:super(x, y, tostring(score or 0).."*"..tostring(multiplier or 0))
+    gameHelper:addScore(score, multiplier)
 end
 
 return scoreIndicator
