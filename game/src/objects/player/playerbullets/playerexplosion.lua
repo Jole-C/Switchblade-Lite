2b177902f3f92a7621dd9553abf825a6ad3ebdcb
Noba <jolechallinor3@gmail.com>
2024-07-15 23:04:36 +0100

    Player explosion now spawns alert object

--- game/src/objects/player/playerbullets/playerexplosion.lua
+++ game/src/objects/player/playerbullets/playerexplosion.lua
@@ -1,5 +1,6 @@
 local explosion = require "src.objects.bullet.explosion"
 local scoreObject = require "src.objects.score.scoreindicator"
+local worldAlertObject = require "src.objects.stagedirector.worldalertobject"
 
 local playerExplosion = class({name = "Player Explosion", extends = explosion})
 
@@ -7,7 +8,8 @@ function playerExplosion:new(x, y, radius, damage)
     self:super(x, y, radius, damage, "line", game.manager.currentPalette.playerColour)
     self.lifetime = 0.6
     
-    gameHelper:addGameObject(scoreObject(self.position.x, self.position.y, 1500, gameHelper:getScoreManager().scoreMultiplier))
+    gameHelper:addGameObject(scoreObject(self.position.x, self.position.y + 30, 1500, gameHelper:getScoreManager().scoreMultiplier))
+    gameHelper:addGameObject(worldAlertObject(self.position.x, self.position.y, "Boost Explosion Bonus!", "fontScore"))
 end
 
 function playerExplosion:handleExplosion()
